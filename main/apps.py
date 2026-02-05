5e7d0710a66d1d2e2f0652e21e12c5f00b38b6c6
Mika7o7 <mikaohanyan881@gmail.com>
2026-02-05 16:04:54 +0300

    fix main/app

--- main/apps.py
+++ main/apps.py
@@ -1,17 +1,5 @@
 from django.apps import AppConfig
-import firebase_admin
-from firebase_admin import credentials
-import os
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
-
-    def ready(self):
-        # Инициализируем Firebase только один раз
-        if not firebase_admin._apps:  # проверка, что ещё не инициализировано
-            BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-            cred_path = os.path.join(BASE_DIR, 'main', 'biokam_key.json')
-            cred = credentials.Certificate(cred_path)
-            firebase_admin.initialize_app(cred)
-            print("Firebase Admin успешно инициализирован")
